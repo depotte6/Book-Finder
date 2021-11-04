e5a556790999e4b31e6ce21ba79bdc538cd14d6b
depotte6 <depotte6@gmail.com>
2021-11-04 14:18:29 -0400

    typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql `
+    type Book {
+        _id: ID
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Query {
+        me: User
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    input saveBookInput {
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        forSale: String
+        link: String
+        title: String
+    }
+    type Mutation {
+        login(email: String!, password: String!): auth
+        addUser(username: String!, email: Strin!, password: String!): Auth
+        saveBook(book: saveBookInput): User
+        removeBook(bookId: String!): User
+    }
+`;
+
+module.exports = typDefs;
